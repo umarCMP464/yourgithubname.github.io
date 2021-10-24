0384cb551a6b9428f840f9b295f85d2d7b6b8994
Umar Ali <umar.ali@lc.cuny.edu>
2021-10-24 08:12:04 -0700

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p> 
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/umarCMP464"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me out!
         </a>
       </header>
     </div>
